84176805afbb804c60612ca1c8f43b5062feb37d
Navin Singh <50445729+RussianVirus@users.noreply.github.com>
2025-02-06 18:18:33 +0530

    hospital fixed

--- hospital.py
+++ hospital.py
@@ -18,16 +18,20 @@ def main(inputpath):
         '>= 500': [500, 1000],
         '250 - 499': [250, 499],
     }
+    tf = {
+        'f': False,
+        't': True
+    }
     for _, row in df.iterrows():
         uid = ex[f"Hospital{row[cols[0]]}"]
         g.add((uid, RDF.type, eicu.Hospital))
         if pd.notnull(row[cols[1]]):
-            g.add((uid, eicu["hasMinNumBedCategory"], mapper[row[cols[1]]][0]))
-            g.add((uid, eicu["hasMaxNumBedCategory"], mapper[row[cols[1]]][1]))
+            g.add((uid, eicu["hasMinNumBedCategory"], Literal(mapper[row[cols[1]]][0], datatype=XSD.integer)))
+            g.add((uid, eicu["hasMaxNumBedCategory"], Literal(mapper[row[cols[1]]][1], datatype=XSD.integer)))
         if pd.notnull(row[cols[2]]):
-            g.add((uid, eicu["hasTeachingStatus"], row[cols[2]]))
+            g.add((uid, eicu["hasTeachingStatus"], Literal(tf[row[cols[2]]], datatype=XSD.boolean)))
         if pd.notnull(row[cols[3]]):
-            g.add((uid, eicu["hasRegion"], row[cols[3]]))
+            g.add((uid, eicu["hasRegion"], Literal(row[cols[3]])))
 
     g.serialize("result/hospital.ttl", format="turtle")
     return
