c5840b68d8c51ebd6623289da8c097bb6e673187
Navin Singh <50445729+RussianVirus@users.noreply.github.com>
2025-02-06 17:59:43 +0530

    carePlanGoal fixed

--- carePlanGoal.py
+++ carePlanGoal.py
@@ -18,9 +18,9 @@ def main(inputpath):
         g.add((uid, RDF.type, eicu.CarePlanGoal))
         g.add((ex[f"PatientUnitStay{row[cols[1]]}"], eicu.hascarePlanGoal, uid))
         g.add((uid, eicu["hasCarePlanGoalOffset"], Literal(row[cols[2]], datatype=XSD.integer)))
-        g.add((uid, eicu["hasCarePlanGoalCategory"], row[cols[3]]))
-        g.add((uid, eicu["hasCarePlanGoalValue"], row[cols[4]]))
-        g.add((uid, eicu["hasCarePlanGoalStatus"], row[cols[5]]))
+        g.add((uid, eicu["hasCarePlanGoalCategory"], Literal(row[cols[3]])))
+        g.add((uid, eicu["hasCarePlanGoalValue"], Literal(row[cols[4]])))
+        g.add((uid, eicu["hasCarePlanGoalStatus"], Literal(row[cols[5]])))
         g.add((uid, eicu["hasActiveUponDischarge"], Literal(row[cols[6]], datatype=XSD.boolean)))
         
     g.serialize("result/carePlanGoal.ttl", format="turtle")
