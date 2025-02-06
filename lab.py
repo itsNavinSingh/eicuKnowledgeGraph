dbece126a6d7f23b7943d8b5c2b2e03c1351a3cb
Navin Singh <50445729+RussianVirus@users.noreply.github.com>
2025-02-06 18:42:39 +0530

    lab fixed

--- lab.py
+++ lab.py
@@ -12,6 +12,7 @@ def main(inputpath):
     # lab
     df = pd.read_csv(f"{inputpath}/lab.csv")
     cols = df.columns
+    i=0
     for _, row in df.iterrows():
         uid = ex[f"Lab{row[cols[0]]}"]
         g.add((uid, RDF.type, eicu.Lab))
@@ -21,15 +22,19 @@ def main(inputpath):
         if pd.notnull(row[cols[3]]):
             g.add((uid, eicu["hasLabTypeId"], Literal(row[cols[3]], datatype=XSD.integer)))
         if pd.notnull(row[cols[4]]):
-            g.add((uid, eicu["hasLabName"], row[cols[4]]))
+            g.add((uid, eicu["hasLabName"], Literal(row[cols[4]])))
         if pd.notnull(row[cols[5]]):
             g.add((uid, eicu["hasLabResult"], Literal(row[cols[5]], datatype=XSD.float)))
         if pd.notnull(row[cols[7]]):
-            g.add((uid, eicu["hasLabMeasureNameSystem"], row[cols[7]]))
+            g.add((uid, eicu["hasLabMeasureNameSystem"], Literal(row[cols[7]])))
         if pd.notnull(row[cols[8]]):
-            g.add((uid, eicu["hasLabMeasureNameInterface"], row[cols[8]]))
+            g.add((uid, eicu["hasLabMeasureNameInterface"], Literal(row[cols[8]])))
         if pd.notnull(row[cols[9]]):
             g.add((uid, eicu["hasLabResultRevisedOffset"], Literal(row[cols[9]], datatype=XSD.integer)))
+        if(i==100):
+            print("Done")
+            return
+        i += 1
 
     g.serialize("result/lab.ttl", format="turtle")
     return
